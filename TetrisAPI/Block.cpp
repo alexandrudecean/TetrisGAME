91b7d12c10f437bf4b7015fe2c406b66de3df660
IoanaMitrofan <ioana.mitrofan@student.unitbv>
2024-11-13 19:28:41 +0200

    Implemented methods

--- TetrisAPI/Block.cpp
+++ TetrisAPI/Block.cpp
@@ -2,20 +2,34 @@
 
 using namespace TetrisAPI;
 
+Block::Block(const Color& color, EBlockType blockType):m_color(color),m_blockType(blockType)
+{
+}
+
 void Block::Rotate()
 {
+	indexState++;
+	if (indexState == (int)m_rotation.size())
+	{
+		indexState = 0;
+	}
 }
 
 void Block::UndoRotate()
 {
+	indexState--;
+	if (indexState < 0)
+	{
+		indexState = (int)m_rotation.size()-1;
+	}
 }
 
 std::vector<Position> Block::GetCurrentRotation() const
 {
-	return std::vector<Position>();
+	return m_rotation[indexState];
 }
 
 Color Block::GetColor() const
 {
-	return Color(0, 0, 0, 0);
+	return m_color;
 }
--- TetrisAPI/Block.h
+++ TetrisAPI/Block.h
@@ -2,18 +2,24 @@
 #include <vector>
 #include "Position.h"
 #include "Color.h"
+#include "IColorManager.h"
+#include "EBlockType.h"
 
 namespace TetrisAPI
 {
 	class Block
 	{
 	public:
+		Block(const Color& color, EBlockType blockType);
 		void Rotate();
 		void UndoRotate();
 		std::vector<Position> GetCurrentRotation() const;
 		Color GetColor() const;
 	private:
 		std::vector<std::vector<Position>> m_rotation;
+		int indexState;
+		Color m_color;
+		EBlockType m_blockType;
 	};
 }
 
