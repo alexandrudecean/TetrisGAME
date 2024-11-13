7e5b5d547dcc8f509b6f0adeac978de7ed40b826
FilipPopescu <filippopescu18@gmail.com>
2024-11-13 14:18:46 +0200

    Remade the test project and added the first test

--- TetrisAPI/Block.cpp
+++ TetrisAPI/Block.cpp
@@ -1,3 +1,21 @@
 #include "Block.h"
 
 using namespace TetrisAPI;
+
+void Block::Rotate()
+{
+}
+
+void Block::UndoRotate()
+{
+}
+
+std::vector<Position> Block::GetCurrentRotation() const
+{
+	return std::vector<Position>();
+}
+
+Color Block::GetColor() const
+{
+	return Color(0, 0, 0, 0);
+}
