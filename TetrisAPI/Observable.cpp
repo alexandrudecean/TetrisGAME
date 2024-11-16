a4ce5437ff04a3dfee0aa2502dd44bcbde2d2e75
FilipPopescu <filippopescu18@gmail.com>
2024-11-16 22:33:48 +0200

    Implemented Observable with weak_ptr

--- TetrisAPI/Observable.cpp
+++ TetrisAPI/Observable.cpp
@@ -1,14 +1,41 @@
 #include "Observable.h"
 using namespace TetrisAPI;
 
-void Observable::Register(IObserver* observer)
+void Observable::Register(const std::weak_ptr<IObserver>& observer)
 {
+    m_observers.emplace_back(observer);
 }
 
-void Observable::Remove(IObserver* observer)
+template <typename T, typename U>
+bool equals(const std::weak_ptr<T>& t, const std::weak_ptr<U>& u)
 {
+    return !t.owner_before(u) && !u.owner_before(t);
+}
+
+void Observable::Remove(const std::weak_ptr<IObserver>& observer)
+{
+    m_observers.erase(
+        std::remove_if(
+            m_observers.begin(),
+            m_observers.end(),
+            [&](const std::weak_ptr<IObserver>& registeredObserver) {
+                return equals(observer, registeredObserver);
+            }),
+        m_observers.end());
 }
 
 void Observable::NotifyAll(uint16_t numLines)
 {
+    for (auto it = m_observers.begin(); it != m_observers.end();)
+    {
+        if (auto sp = it->lock())
+        {
+            sp->OnLinesCleared(numLines);
+            ++it;
+        }
+        else
+        {
+            it = m_observers.erase(it);
+        }
+    }
 }
--- TetrisAPI/Observable.h
+++ TetrisAPI/Observable.h
@@ -1,15 +1,18 @@
 #pragma once
+#include <memory>
+#include <vector>
 #include "IObserver.h"
 
-namespace TetrisAPI {
-
+namespace TetrisAPI 
+{
 	class Observable
 	{
 	public:
-		void Register(IObserver* observer);
-		void Remove(IObserver* observer);
+		void Register(const std::weak_ptr<IObserver>& observer);
+		void Remove(const std::weak_ptr<IObserver>& observer);
 		void NotifyAll(uint16_t numLines);
-
+	private:
+		std::vector<std::weak_ptr<IObserver>> m_observers;
 	};
 }
 
