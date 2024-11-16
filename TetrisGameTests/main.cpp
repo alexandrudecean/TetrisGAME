b289930015ee218515b5928070762122f05e476d
FilipPopescu <filippopescu18@gmail.com>
2024-11-17 00:03:01 +0200

    Added Observer, Timer and Game tests

--- TetrisGameTests/main.cpp
+++ TetrisGameTests/main.cpp
@@ -1,17 +1,172 @@
 #include <gtest/gtest.h>
 #include <gmock/gmock.h>
+#include <thread>
 
-#include "Grid.h"
+#include "Game.h"
 
-using testing::Return;
+//using testing::Return;
+using namespace testing;
 using namespace TetrisAPI;
 
-class ColorManagerMock : public IColorManager
+class IColorManagerMock : public IColorManager
 {
 public:
 	MOCK_METHOD(Color, GetEmptyCellColor, (), (const override));
 	MOCK_METHOD(Color, GetRandomBlockColor, (), (const override));
 };
+
+class IInputManagerMock : public IInputManager
+{
+public:
+	MOCK_METHOD(bool, Check, (EInputType inputType), (override));
+	MOCK_METHOD(void, Register, (EInputType inputType, uint16_t key), (override));
+};
+
+class IObserverMock : public IObserver
+{
+public:
+	MOCK_METHOD(void, OnLinesCleared, (uint16_t linesCleared), (override));
+};
+
+namespace GameTests
+{
+	TEST(GameTest, Constructor)
+	{
+		auto inputManagerMock = std::make_shared<IInputManagerMock>();
+		auto colorManagerMock = std::make_shared<IColorManagerMock>();
+		Color color(0, 0, 0, 0);
+
+		EXPECT_CALL(*colorManagerMock, GetEmptyCellColor())
+			.WillOnce(Return(color));
+		EXPECT_CALL(*colorManagerMock, GetRandomBlockColor())
+			.WillOnce(Return(color));
+
+		Game game(colorManagerMock, inputManagerMock);
+	}
+
+	TEST(GameTest, FirstUpdateGetsCalled)
+	{
+		auto inputManagerMock = std::make_shared<IInputManagerMock>();
+		auto colorManagerMock = std::make_shared<IColorManagerMock>();
+		Color color(0, 0, 0, 0);
+
+		EXPECT_CALL(*colorManagerMock, GetEmptyCellColor())
+			.WillOnce(Return(color));
+		EXPECT_CALL(*colorManagerMock, GetRandomBlockColor())
+			.Times(2)
+			.WillRepeatedly(Return(color));
+
+		Game game(colorManagerMock, inputManagerMock);
+		game.Update();
+	}
+}
+
+namespace TimerTests
+{
+	TEST(TimerTest, Constructor)
+	{
+		Timer timer(1);
+
+		ASSERT_EQ(timer.IsActive(), false);
+	}
+
+	TEST(TimerTest, StartStopWorks)
+	{
+		Timer timer(1);
+		timer.Start();
+		
+		ASSERT_EQ(timer.IsActive(), true);
+		timer.Stop();
+		ASSERT_EQ(timer.IsActive(), false);
+	}
+
+	TEST(TimerTest, GetElapsedTimeWorks)
+	{
+		Timer timer(3);
+		timer.Start();
+
+		std::this_thread::sleep_for(2s);
+		
+		ASSERT_EQ(timer.GetElapsedTime().count(), 2);
+		ASSERT_EQ(timer.IsActive(), true);
+	}
+
+	TEST(TimerTest, ReachedThreshold)
+	{
+		Timer timer(3);
+		timer.Start();
+
+		std::this_thread::sleep_for(3s);
+		
+		ASSERT_EQ(timer.ReachedThreshold(), true);
+	}
+}
+
+namespace ObservableTests
+{
+	TEST(ObservableTest, Register)
+	{
+		Observable observable;
+		uint16_t linesCleared = 2;
+		std::shared_ptr<IObserverMock> observerMock = std::make_shared<IObserverMock>();
+		
+		EXPECT_CALL(*observerMock, OnLinesCleared(linesCleared))
+			.Times(1);
+
+		observable.Register(observerMock);
+
+		observable.NotifyAll(linesCleared);
+	}
+
+	TEST(ObservableTest, Remove)
+	{
+		Observable observable;
+		uint16_t linesCleared = 2;
+		std::shared_ptr<IObserverMock> observerMock = std::make_shared<IObserverMock>();
+
+		EXPECT_CALL(*observerMock, OnLinesCleared(linesCleared))
+			.Times(0);
+
+		observable.Register(observerMock);
+		observable.Remove(observerMock);
+
+		observable.NotifyAll(linesCleared);
+	}
+
+	TEST(ObservableTest, NotifyAllGetsCalledForAllObservers)
+	{
+		Observable observable;
+		uint16_t linesCleared = 2;
+		std::shared_ptr<IObserverMock> observerMock1 = std::make_shared<IObserverMock>();
+		std::shared_ptr<IObserverMock> observerMock2 = std::make_shared<IObserverMock>();
+
+		EXPECT_CALL(*observerMock1, OnLinesCleared(linesCleared));
+		EXPECT_CALL(*observerMock2, OnLinesCleared(linesCleared));
+
+		observable.Register(observerMock1);
+		observable.Register(observerMock2);
+
+		observable.NotifyAll(linesCleared);
+	}
+
+	TEST(ObservableTest, NotifyAllOnlyGetsCalledForAlivePointers)
+	{
+		Observable observable;
+		uint16_t linesCleared = 2;
+		std::weak_ptr<IObserverMock> observerMockWeak;
+		{
+			std::shared_ptr<IObserverMock> observerMock = std::make_shared<IObserverMock>();
+			observerMockWeak = observerMock;
+			
+			EXPECT_CALL(*observerMock, OnLinesCleared(linesCleared))
+				.Times(0);
+		}
+
+		observable.Register(observerMockWeak);
+
+		observable.NotifyAll(linesCleared);
+	}
+}
 
 namespace GridTests 
 {
@@ -30,7 +185,8 @@ namespace GridTests
 		return gridMatrix;
 	}
 
-	TEST(GridTest, CheckConstructor) {
+	TEST(GridTest, CheckConstructor) 
+	{
 		Color emptyCellColor(0, 0, 0, 0);
 		auto expectedMatrixGrid = CreateEmptyGridMatrix(emptyCellColor);
 
